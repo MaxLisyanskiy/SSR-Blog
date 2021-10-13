435507dad34fca333ed6c6435999b3861be0d816
Максим Лисянский <max_lisyanskiy@mail.ru>
2021-10-13 22:26:08 +0300

    finished styled app-post max 13.10

---
+++ components/client/AppPost.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>post</h3>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+
+    <div class="post-body">
+      <img 
+        src="" 
+        alt="" 
+        class="post-img"
+      >
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+      <span>
+        <i class="el-icon-chat-dot-round"></i>
+        <span>12</span>
+      </span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 25px;
+    
+    &-img {
+      width: 100%;
+      height: auto;
+    }
+    &-footer {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1rem;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <AppPost 
+        v-for="post in 3" :key="post"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import AppPost from '~/components/client/AppPost'
+
 export default {
+  head: {
+    title: 'Главная'
+  },
+  components: {
+    AppPost
+  }
 }
 </script>
